6b6e23ba4331f006b1dfd3e04fd0667e177f084d
Nihal Padwal <nihalpadwal@gmail.com>
2023-12-25 09:47:22 +0530

    added posts model

--- router/route.js
+++ router/route.js
@@ -7,20 +7,25 @@ import { registerMail } from "../controllers/mailer.js";
 import Auth, { localVariables } from "../middleware/auth.js";
 
 // POST METHODS
+// AUTH
 router.route("/register").post(controller.register); // register user
 router.route("/registerMail").post(registerMail); // send the email
 router
   .route("/autenticate")
   .post(controller.verifyUser, (req, res) => res.end()); // authenticate user
 router.route("/login").post(controller.verifyUser, controller.login); // login in app
+// POSTS
+router.route("/createPost").post(controller.verifyUser, controller.createPost); // created posts
 
 // GET METHODS
+// AUTH
 router.route("/user").get(controller.getUser); // user with response
 router.route("/generateOTP").get(controller.generateOTP); // generate random OTP
 router.route("/verifyOTP").get(controller.verifyOTP); // verify generate OTP
 router.route("/createResetSession").get(controller.createResetSession); // reset all the variables
 
 // PUT METHODS
+// AUTH
 router.route("/updateuser").put(Auth, controller.updateUser); // is used to update the user profile
 router
   .route("/resetPassword")
